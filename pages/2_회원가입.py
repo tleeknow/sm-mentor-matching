0e29b5fa6e302bd14bceff55355078a7ea101663
tleeknow <yejinan800@gmail.com>
2026-07-23 12:59:43 +0900

    Implement user registration form with validation

--- pages/2_회원가입.py
+++ pages/2_회원가입.py
@@ -1,1 +1,42 @@
+import streamlit as st
 
+st.title("📝 회원가입")
+
+name = st.text_input("이름")
+
+student_id = st.text_input("학번")
+
+email = st.text_input("숙명대학교 이메일")
+
+password = st.text_input(
+    "비밀번호",
+    type="password"
+)
+
+password2 = st.text_input(
+    "비밀번호 확인",
+    type="password"
+)
+
+if st.button("다음"):
+
+    if (
+        name == "" or
+        student_id == "" or
+        email == "" or
+        password == "" or
+        password2 == ""
+    ):
+        st.error("모든 정보를 입력해주세요.")
+
+    elif password != password2:
+        st.error("비밀번호가 일치하지 않습니다.")
+
+    elif not email.endswith("@sookmyung.ac.kr"):
+        st.error("숙명대학교 이메일만 가입 가능합니다.")
+
+    else:
+
+        st.success("입력 완료!")
+
+        st.switch_page("pages/3_이메일인증.py")
